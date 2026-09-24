87056316009722d947dcba29b250625222ea659e
Raphael123836 <rlandryversailles@gmail.com>
2026-09-24 14:06:01 -0400

    Test_initial

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,9 @@
 
 void setup() 
 {
-  // Initialise le port de communication et attend pour l'ouvrir:
-  Serial.begin(9600);
-  // Ce délai permet de s'assurer que le moniteur serie (Serial Monitor) soit disponible
-  delay(1500);
+
 }
 
 void loop() 
 {
-  Serial.println("Bonjour le monde!");
-
-  delay(100);
 }
